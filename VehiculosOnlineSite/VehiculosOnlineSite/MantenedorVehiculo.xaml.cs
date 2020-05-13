782e4a34abaf9f1e82a7afc212404f0512fc0d42
Carlos Guerra Quiroz <carlosdavidgq@gmail.com>
2020-05-13 19:19:53 -0400

    respaldo, mantenedor vehiculo y login

---
+++ VehiculosOnlineSite/VehiculosOnlineSite/MantenedorVehiculo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VehiculosOnlineSite
+{
+    /// <summary>
+    /// Lógica de interacción para MantenedorVehiculo.xaml
+    /// </summary>
+    public partial class MantenedorVehiculo : Window
+    {
+        public MantenedorVehiculo()
+        {
+            InitializeComponent();
+        }
+    }
+}
