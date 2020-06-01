6e3fa87c5b71b2e51490aa4ded501fd310eeb841
Carlos David Guerra Quiroz <cguerraq@sonda.com>
2020-06-01 19:27:45 -0400

    respaldo

--- Site/VehiculosOnlineSite/VehiculosOnlineSite/ListarVehiculo.xaml.cs
+++ Site/VehiculosOnlineSite/VehiculosOnlineSite/ListarVehiculo.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using VehiculosOnlineSite.BLL;
 
 namespace VehiculosOnlineSite
 {
@@ -19,9 +20,57 @@ namespace VehiculosOnlineSite
     /// </summary>
     public partial class ListarVehiculo : Window
     {
+        private readonly MarcaBL _marcaBl = new MarcaBL();
+        private readonly VehiculoBL _vehiculoBl = new VehiculoBL();
         public ListarVehiculo()
         {
-            InitializeComponent();
+            try
+            {
+                WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                InitializeComponent();
+                ObtenerMarcas();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: \r\n {ex.Message}", "Ocurrió un error");
+            }
+        }
+
+        private void btnCrear_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ObtenerMarcas()
+        {
+            this.cboMarca.ItemsSource = _marcaBl.ObtenerMarcas();
+            this.cboMarca.DisplayMemberPath = "Nombre";
+            this.cboMarca.SelectedValuePath = "Id";
+            this.cboMarca.SelectedIndex = 0;
+        }
+
+        private void ObtenerModelos(int idMarca)
+        {
+            this.cboMarca.ItemsSource = _marcaBl.ObtenerModelos(idMarca);
+            this.cboMarca.DisplayMemberPath = "Nombre";
+            this.cboMarca.SelectedValuePath = "Id";
+            this.cboMarca.SelectedIndex = 0;
+        }
+
+        private void cboMarca_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                var idMarca = this.cboMarca.SelectedValue;
+                if (idMarca != null)
+                {
+                    ObtenerModelos(int.Parse(idMarca.ToString()));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: \r\n {ex.Message}", "Ocurrió un error");
+            }
         }
     }
 }
